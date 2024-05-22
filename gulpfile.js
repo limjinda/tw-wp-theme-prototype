cd6073aaddca6a5106b59e959753e0b690398927
Jirayu Limjinda <jir4yu.l@gmail.com>
2024-05-22 09:11:15 +0700

    update vendor script/style

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,14 @@ const browserSync = require('browser-sync').create()
 const terser = require('gulp-terser')
 const sourcemaps = require('gulp-sourcemaps')
 const imagemin = require('gulp-imagemin')
-
-const reload = browserSync.reload
-const exec = require('child_process').exec
+const concat = require('gulp-concat')
+const cleanCSS = require('gulp-clean-css')
 
 const ts = require('gulp-typescript')
 const tsProject = ts.createProject('tsconfig.json')
+
+const reload = browserSync.reload
+const exec = require('child_process').exec
 
 const scripts = () => {
   let tsResult = gulp.src('src/**/*.ts').pipe(tsProject())
@@ -29,7 +31,6 @@ const tw = (done) => {
     'npx tailwindcss -i ./styles/global.css -o ./style.css --minify',
     (err) => {
       reload()
-      done(err)
     }
   )
 }
@@ -41,12 +42,45 @@ const optimizeImages = () => {
     .pipe(gulp.dest('./img'))
 }
 
-gulp.task('default', function () {
-  return scripts()
-})
+const vendorStyles = () => {
+  return gulp
+    .src(
+      [
+        './node_modules/slick-carousel/slick/slick.css',
+        './node_modules/slick-carousel/slick/slick-theme.css',
+      ],
+      { allowEmpty: true }
+    )
+    .pipe(concat('vendor.css'))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('./dist/vendor'))
+}
+
+const vendorScripts = () => {
+  return gulp
+    .src(['./node_modules/slick-carousel/slick/slick.js'], { allowEmpty: true })
+    .pipe(
+      concat('vendor.js', {
+        newLine: ';',
+      })
+    )
+    .pipe(
+      terser({
+        output: { preserve_annotations: true, comments: false },
+        compress: { drop_console: true, drop_debugger: true },
+      })
+    )
+    .pipe(gulp.dest('./dist/vendor'))
+}
 
 gulp.task('images', function () {
   return optimizeImages()
+})
+
+gulp.task('vendor', function () {
+  vendorStyles()
+  vendorScripts()
+  return Promise.resolve(':: done ::')
 })
 
 gulp.task('watch', function () {
@@ -56,6 +90,5 @@ gulp.task('watch', function () {
     notify: false,
   })
   gulp.watch('src/**/*.ts', scripts)
-  gulp.watch(['*.php', '**/*.php']).on('change', reload)
-  gulp.watch('styles/global.css', tw)
+  gulp.watch(['*.php', '**/*.php', 'styles/*.css']).on('change', tw)
 })
